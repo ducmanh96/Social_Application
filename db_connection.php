ffe91951431e766df30a216d756d29cd8fdd1cad
DO DUC MANH <ducmanh96.vp@gmail.com>
2024-05-29 00:58:35 +0900

    finshised

--- db_connection.php
+++ db_connection.php
@@ -1,11 +1,11 @@
 <?php
-$servername = getenv('DB_HOST');
+$hostname = getenv('DB_HOST');
 $username = getenv('DB_USER');
 $password = getenv('DB_PASSWORD');
 $dbname = getenv('DB_NAME');
 
 // Tạo kết nối
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($hostname, $username, $password, $dbname);
 
 // Kiểm tra kết nối
 if ($conn->connect_error) {
