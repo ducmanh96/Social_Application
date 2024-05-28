6d5bb6b007f70193c64b2204cb1ec96f7223ff32
DO DUC MANH <ducmanh96.vp@gmail.com>
2024-05-29 01:22:07 +0900

    finshied

--- db_connection.php
+++ db_connection.php
@@ -1,5 +1,6 @@
 <?php
-$hostname = getenv('DB_HOST');
+// Lấy thông tin kết nối từ biến môi trường
+$servername = getenv('DB_HOST');
 $username = getenv('DB_USER');
 $password = getenv('DB_PASSWORD');
 $dbname = getenv('DB_NAME');
