ec03a2b60043c66d834f69d03cb0db0c54f84d25
DO DUC MANH <ducmanh96.vp@gmail.com>
2024-05-28 23:49:31 +0900

    修正した

--- db_connection.php
+++ db_connection.php
@@ -1,12 +1,15 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "SNSApp";
+$servername = getenv('DB_HOST');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
+$dbname = getenv('DB_NAME');
 
+// Tạo kết nối
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+// Kiểm tra kết nối
 if ($conn->connect_error) {
-    die("接続に失敗しました： " . $conn->connect_error);
+    die("Kết nối thất bại: " . $conn->connect_error);
 }
+echo "Kết nối thành công";
 ?>
